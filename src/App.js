0009dbd9d6e497a8c624b5c9a185f12c64685dc3
heshaniadithya <heshaniadithya2006@gmail.com>
2024-04-23 13:52:35 +0530

    drag-n-drop

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import Bin from './components/Bin';
+import Note from './components/Note';
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  const [binnedItems, setBinnedItems] = useState([]);
+
+  useEffect(() => {
+    const initialNotes = ["Note 1", "Note 2", "Note 3"];
+    localStorage.setItem("noteList", JSON.stringify(initialNotes))
+
+    let array = localStorage.getItem("noteList");
+    setNotes(JSON.parse(array));
+  }, [])
+
+  useEffect(() => {
+    let array = localStorage.getItem("binnedItems");
+
+    if (array) {
+      setBinnedItems(JSON.parse(array));
+      for (let i = 0; i < notes.length; i++) {
+        for (let j = 0; j < binnedItems.length; j++) {
+          if (notes[i] === binnedItems[j]) {
+            notes.splice(i, 1)
+          }
+        }
+      }
+    }
+  }, [notes])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DndProvider backend={HTML5Backend}>
+    <h1 className="text-center text-3xl font-semibold mt-4 py-2">Drag-n-Drop</h1>
+    {notes.map(item =>
+      <Note 
+      key={new Date().getTime() + Math.floor(Math.random() * 1000)}
+      note={item}
+      binnedItems={binnedItems}
+      />
+    )}
+    <Bin binnedItems={binnedItems}/>
+    </DndProvider>
   );
 }
 
